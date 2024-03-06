f10ff616c7d26b76a9b9a3f6033936d8db8d1857
Laio <laerciosoaresfragoso@gmail.com>
2024-03-06 11:56:46 -0300

    Alinhamentos e Arranjos

--- AndroidStudioProjects/Desafio01/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ AndroidStudioProjects/Desafio01/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,10 +41,30 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+
+    Row(
+        modifier = modifier.fillMaxSize(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+
+        Text(text = "Texto 01")
+        Text(text = "Testo 02")
+
+        Column(
+            modifier = modifier
+                .background(Color.Cyan)
+                .width(150.dp)
+                .height(150.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+
+        ) {
+            Text(text = "ABC")
+            Text(text = "XYZ")
+        }
+
+    }
 }
 
 @Preview(showBackground = true)

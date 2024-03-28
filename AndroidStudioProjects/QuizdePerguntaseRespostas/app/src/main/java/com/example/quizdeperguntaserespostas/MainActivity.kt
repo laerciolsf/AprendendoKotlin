a526cb1f45bb93bc375f06ba8c14aa4530a9d0e4
Laio <laerciosoaresfragoso@gmail.com>
2024-03-28 15:34:22 -0300

    Add botão de voltar ao inicio no QuizGAme

--- AndroidStudioProjects/QuizdePerguntaseRespostas/app/src/main/java/com/example/quizdeperguntaserespostas/MainActivity.kt
+++ AndroidStudioProjects/QuizdePerguntaseRespostas/app/src/main/java/com/example/quizdeperguntaserespostas/MainActivity.kt
@@ -52,12 +52,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MinhaTela() {
-    Column (
+    Column(
         modifier = Modifier.fillMaxSize(), // Ocupa toda a tela
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally)
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
     {
-        var questionarioIniciado by remember { mutableStateOf(false) }
+        /*var questionarioIniciado by remember { mutableStateOf(false) }
 
         if (!questionarioIniciado) {
             Button(
@@ -68,6 +69,8 @@ fun MinhaTela() {
         } else {
             JogoQuiz()
         }
+         */
+        JogoQuiz()
     }
 }
 
@@ -76,6 +79,8 @@ fun JogoQuiz() {
     var indicePerguntaAtual by remember { mutableStateOf(0) }
     var respostaSelecionada by remember { mutableStateOf<String?>(null) }
     var respostasCorretas by remember { mutableStateOf(0) }
+    var resultado: String by remember { mutableStateOf("") }
+    var exibirTelaInicial by remember { mutableStateOf(true) }
 
     val perguntas = listOf(
         "Qual é a capital do Brasil?" to listOf(
@@ -115,64 +120,100 @@ fun JogoQuiz() {
         listOf("E")  // Resposta correta 4
     )
 
-    var resultado: String by remember { mutableStateOf("") }
-
     Column(
         modifier = Modifier.padding(20.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.Start
     ) {
-        if (resultado.isNotEmpty()) {
-            // Se o resultado não estiver vazio, mostrar apenas o resultado
-            Text(
-                text = resultado,
-                modifier = Modifier.padding(16.dp)
-            )
-        } else {
-            // Caso contrário, mostrar a pergunta e as opções de resposta
-            Text(
-                text = perguntas[indicePerguntaAtual].first,
-                modifier = Modifier.padding(16.dp)
-            )
-
-            perguntas[indicePerguntaAtual].second.forEach { opcao ->
-                Row(
-                    modifier = Modifier.padding(8.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    RadioButton(
-                        selected = (respostaSelecionada == opcao.substring(0, 1)),
-                        onClick = { respostaSelecionada = opcao.substring(0, 1) }
-                    )
-                    Text(text = opcao, modifier = Modifier.padding(start = 8.dp))
-                }
-            }
-
-            Spacer(modifier = Modifier.height(16.dp))
-
+        if (exibirTelaInicial) {
+            // Se exibirTelaInicial for true, mostrar a tela inicial
             Button(
                 onClick = {
-                    // Verifica se a resposta selecionada está correta
-                    if (respostaSelecionada in respostasCorretasEsperadas[indicePerguntaAtual]) {
-                        respostasCorretas++
-                    }
-
-                    // Passa para a próxima pergunta ou exibe o resultado final
-                    if (indicePerguntaAtual < perguntas.size - 1) {
-                        indicePerguntaAtual++
-                        respostaSelecionada = null // Limpa a resposta selecionada para a próxima pergunta
-                    } else {
-                        // Exibe o resultado final
-                        resultado = "Você acertou $respostasCorretas de ${perguntas.size} perguntas!"
-                    }
+                    exibirTelaInicial = false
+                    resultado = "" // Limpar o resultado ao voltar para o início
+                    indicePerguntaAtual = 0 // Reiniciar o índice da pergunta
+                    respostaSelecionada = null // Limpar a resposta selecionada
+                    respostasCorretas = 0 // Reiniciar o contador de respostas corretas
                 }
             ) {
-                if (indicePerguntaAtual < perguntas.size - 1) {
-                    Text(text = "Próxima Pergunta")
-                } else {
-                    Text(text = "Verificar Resultado")
+                Text(text = "Iniciar Jogo")
+            }
+        } else {
+            if (resultado.isNotEmpty()) {
+                // Se o resultado não estiver vazio, mostrar apenas o resultado
+                Text(
+                    text = resultado,
+                    modifier = Modifier.padding(16.dp)
+                )
+
+
+
+
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Botão para voltar ao início
+                Button(
+                    onClick = {
+                        exibirTelaInicial = true
+                    }
+                ) {
+                    Text(text = "Voltar ao Início")
+                }
+
+
+
+
+            } else {
+                // Caso contrário, mostrar a pergunta e as opções de resposta
+                Text(
+                    text = perguntas[indicePerguntaAtual].first,
+                    modifier = Modifier.padding(16.dp)
+                )
+
+                perguntas[indicePerguntaAtual].second.forEach { opcao ->
+                    Row(
+                        modifier = Modifier.padding(8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RadioButton(
+                            selected = (respostaSelecionada == opcao.substring(0, 1)),
+                            onClick = { respostaSelecionada = opcao.substring(0, 1) }
+                        )
+                        Text(text = opcao, modifier = Modifier.padding(start = 8.dp))
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Button(
+                    onClick = {
+                        // Verifica se a resposta selecionada está correta
+                        if (respostaSelecionada in respostasCorretasEsperadas[indicePerguntaAtual]) {
+                            respostasCorretas++
+                        }
+
+                        // Passa para a próxima pergunta ou exibe o resultado final
+                        if (indicePerguntaAtual < perguntas.size - 1) {
+                            indicePerguntaAtual++
+                            respostaSelecionada =
+                                null // Limpa a resposta selecionada para a próxima pergunta
+                        } else {
+                            // Exibe o resultado final
+                            resultado =
+                                "Você acertou $respostasCorretas de ${perguntas.size} perguntas!"
+                        }
+                    }
+                ) {
+                    if (indicePerguntaAtual < perguntas.size - 1) {
+                        Text(text = "Próxima Pergunta")
+                    } else {
+                        Text(text = "Verificar Resultado")
+                    }
                 }
             }
+
+//
         }
     }
 }
